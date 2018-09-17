ee216dd89cc610d4d371b009c9c0df4154e0fb37
coder <mshawnlouw@gmail.com>
2018-09-17 11:04:48 +0200

    displayng with my factory function

--- index.js
+++ index.js
@@ -30,53 +30,35 @@ app.set('view engine', 'handlebars');
 
 let regNumberInstance = regNumber();
 
-app.get('/', async function(req, res){
-    // displayRegs:regNumberInstance.forRegMap();
+app.get('/', function(req, res){
+  // displayRegs:regNumberInstance.forRegMap();
 
-  res.render('reg');
+  res.render('reg', {displayRegs:regNumberInstance.forRegMap()});
 });
 
-app.post('/reg_numbers', async function(req, res){
-  try {
+app.post('/reg_numbers', function(req, res){
     let textInput = req.body.text;
     let dropDownCheck = req.body.dropdown;
 
-    if(textInput === "" || textInput === undefined){
-      req.flash("info", "Please enter a registration number!");
-    }
-    else{
-      let typedText = textInput.substring(0,3).trim();
-      let result = await pool.query('select * from towns where town_name=$1', [typedText]);
+    let displayRegs =regNumberInstance.inputReg(textInput);
+    let dropdown = regNumberInstance.forFiltering(dropDownCheck);
 
-      if(result.rows.length !== 0){
-        await pool.query('insert into reg_numbers (towns_id,reg_numbers) values($1,$2)', [result.rows[0].id,typedText] );
-      }
+  res.render('reg', {displayRegs,dropdown}) });
 
-    }
-
-  } catch (err) {
-
-  }
-
-    // let displayRegs =regNumberInstance.inputReg(textInput);
-    // let dropdown = regNumberInstance.forFiltering(dropDownCheck);
-
-  res.render('reg');
-});
-
-app.get('/:towns', async function(req, res){
+app.get('/:towns', function(req, res){
     let town = req.params.towns;
-    // console.log(town);
-    res.render('reg')
-})
+    // console.log(town);	    // console.log(town);
+    res.render('reg', {displayRegs:regNumberInstance.forFiltering(town)} )
+  	})
 
 app.get('/reset', function(req, res){
-  regNumberInstance.reset()
-  console.log(regNumberInstance.reset()
-);
-  res.redirect('/');
+
+  // console.log(  regNumberInstance.reset()	);
+  res.render('reg', {displayRegs:regNumberInstance.reset()});
 })
-let PORT = process.env.PORT || 3099;
+
+
+let PORT = process.env.PORT || 3999;
 app.listen(PORT, function(){
   	console.log('App starting on port', PORT);
 });
--- regNUmbers.js
+++ regNUmbers.js
@@ -1,3 +1,59 @@
-module.exports = function (pool) {
+module.exports = function () {
+  var allRegs =  {};
+  var valid = ["all", "CA", "CEY", "CL", "CJ"];
+  var reg = '';
+  // ca 1235
+  function inputReg(regNumber) {
+  if(allRegs[regNumber] !== ""){
+    if(allRegs[regNumber] == undefined){
+      let tag = regNumber.substring(0,3).trim();
+       if (valid.includes(tag)) {
+        //update reg and add to the maP
+        allRegs[regNumber] = 0;
+        return Object.keys(allRegs);
+      }
+    }
+    return "ENTER ANOTHER ONE!";
+   }
+}
+   function forRegMap() {
+    return Object.keys(allRegs);
+  }
 
+  function returnAll(){
+    if(allRegs === ""){
+      return Object.keys(allRegs);
+    }
+    else if(allRegs !== ""){
+      return allRegs;
+    }
+  }
+   function forIndividual(){
+    return reg;
+  }
+
+   function forFiltering(selectedTown) {
+    var registrations = Object.keys(allRegs);
+    if (selectedTown !== "All") {
+      return registrations.filter(current => current.startsWith(selectedTown));
+    }
+    return registrations;
+  }
+
+  function reset(){
+   let allRegs = {};
+   let reg = "";
+   registrations = "";
+
+ }
+
+   return {
+    inputReg,
+    forIndividual,
+    //forRegNumber,
+    forRegMap,
+    returnAll,
+    // returnAll,
+    forFiltering
+  }
 }
