03e5c224d6095a116b452c007bdde147e786eb75
coder <mshawnlouw@gmail.com>
2018-10-01 16:32:21 +0200

    added some styling(anyana)

--- public/js/functionality.js
+++ public/js/functionality.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.dropdown-trigger');
   var elem = document.querySelectorAll('select');
+  var modal = document.querySelectorAll('.modal');
 
 
   var instances = M.Dropdown.init(elems);
   var instances = M.FormSelect.init(elem);
+  var instances = M.Modal.init(modal);
 
 });
